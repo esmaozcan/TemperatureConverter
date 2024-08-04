dcf2fdf93b30411fcfec0eecfc2b3816d55e4f8e
esmaozcan <esmaozcn41@gmail.com>
2024-08-04 14:24:10 +0300

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Esma Özcan on 4.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
